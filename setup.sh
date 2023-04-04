06c6b7975ca81afded1e4a542ad2a42a9b185c6a
Dingkun Yang <81750079+Yer1k@users.noreply.github.com>
2023-04-04 21:51:19 +0000

    discard gpu option for now

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-source virtualenv ~/.venv
-source ~/.venv/bin/activate
-#append it to bash so every shell launches with it 
-echo 'source ~/.venv/bin/activate' >> ~/.bashrc
-# make install
