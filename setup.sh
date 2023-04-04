09fe74d060f83429d87add1e49d07a3a889cdbed
Dingkun Yang <81750079+Yer1k@users.noreply.github.com>
2023-04-04 20:57:09 +0000

    update env

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ source virtualenv ~/.venv
 source ~/.venv/bin/activate
 #append it to bash so every shell launches with it 
 echo 'source ~/.venv/bin/activate' >> ~/.bashrc
-make install
+# make install
